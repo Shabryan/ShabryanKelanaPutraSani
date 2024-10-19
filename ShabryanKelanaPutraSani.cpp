b4c23d5d4ff9d8dfc17567fc5749775af351a5ac
Shabryan <lanaputrasani@gmail.com>
2024-10-19 13:07:47 +0700

    Add files via upload

---
+++ ShabryanKelanaPutraSani.cpp
@@ -0,0 +1,13 @@
+//Shabryan Kelana Putra Sani
+#include <iostream>
+using namespace std;
+int main() {
+    int nilai;
+       cout<<"Masukan Nilai Anda : ";
+    cin>>nilai;
+      if(nilai>75)
+          cout<<"\nLULUS";
+      else
+         cout<<"\nTIDAK LULUS";
+    return 0;
+}
